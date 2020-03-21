899f72eb23f86c891bcc02833d51654040e1b78e
deepadeepu <deepagorrela@gmail.com>
2020-03-20 18:50:01 -0700

    added new changes

--- lecture3_python flask/application.py
+++ lecture3_python flask/application.py
@@ -1,17 +1,32 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, session
+#from cachelib.file import FileSystemCache
+from flask_session import Session
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config["SESSION_PERMANENT"]= False
+app.config["SESSION_TYPE"]= "filesystem"
+Session(app)
+
+
+@app.route("/",methods=["GET","POST"])
 def index():
-    ##names=["ALice","Bob","Charlie","deepa"]
-    return render_template("index.html")
+    if session.get("notes") is None:
+        session["notes"] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+
+    return render_template("index.html",notes=session["notes"])
 
 
-@app.route("/hello",methods=["POST"])
+@app.route("/hello",methods=["GET","POST"])
 def hello():
-    name = request.form.get("name")
-    return render_template("hello.html",name=name)
+    if request.method == "GET":
+        return"Please Submit form instead"
+    else:
+        name = request.form.get("name")
+        return render_template("hello.html",name=name)
 
 @app.route("/more")
 def more():
